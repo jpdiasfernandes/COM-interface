fc05c31cbd4c24843b32a10e9a221df21033c918
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2024-02-20 16:13:58 +0000

    feat: add https server

--- Interface-COM/app.js
+++ Interface-COM/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
+var
 global.token_blackList = []
 
 var homeRouter = require('./routes/home');
