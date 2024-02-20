841a61c5775c0fe742835f2e14541c0ec611ae21
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2024-02-20 16:20:04 +0000

    fix: typo in app.js

--- Interface-COM/app.js
+++ Interface-COM/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var fs = require('fs');
-var
 global.token_blackList = []
 
 var homeRouter = require('./routes/home');
